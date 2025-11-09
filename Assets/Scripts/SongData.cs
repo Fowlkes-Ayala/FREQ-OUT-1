ad05bdb987895b3fa3cfb076d5609ee2a9590242
William Zhao <williezhao123@gmail.com>
2025-11-09 11:15:19 -0800

    asfasf

---
+++ Assets/Scripts/SongData.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public enum CoinPattern
+    {
+        None,
+        FiveInLine,
+        FiveRandom,
+        TenInLine,
+        ZiGZag
+    }
+    
+    [CreateAssetMenu(fileName = "Song Data", menuName = "Song", order = 0)]
+    public class SongData : ScriptableObject
+    {
+        [SerializeField] private int BPM;
+        [SerializeField, Min(0)] private int totalMeasures;
+
+        [SerializeField] private CoinPattern[] coinPatterns;
+
+        private void OnValidate()
+        {
+            if (totalMeasures < 0) totalMeasures = 0;
+            if (coinPatterns == null || coinPatterns.Length != totalMeasures)
+            {
+                var newArray = new CoinPattern[totalMeasures];
+                if (coinPatterns != null)
+                {
+                    int copyCount = Mathf.Min(coinPatterns.Length, newArray.Length);
+                    System.Array.Copy(coinPatterns, newArray, copyCount);
+                }
+                coinPatterns = newArray;
+            }
+        }
+    }
+}
--- Assets/Scripts/SoundwayManager.cs
+++ Assets/Scripts/SoundwayManager.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Splines;
 
 namespace DefaultNamespace
 {
     public class SoundwayManager : MonoBehaviour
     {
         [SerializeField] private List<Soundway> soundways;
+        [SerializeField] private SplineContainer mainSplineContainer;
         private float swapTime;
         private Soundway currentSoundway;
         private int currentSoundwayIndex = 0;
@@ -24,7 +27,12 @@ namespace DefaultNamespace
                 Instance = this;
             }
             if (soundways == null) Debug.LogError("SoundwayManager is empty!");
+            
             currentSoundway = soundways[currentSoundwayIndex];
+            for (int i = 0; i < soundways.Count; i++)
+            {
+                soundways[i].Spline = mainSplineContainer.Splines[i];
+            }
         }
         
         public Soundway GetCurrentSoundway()
@@ -51,6 +59,17 @@ namespace DefaultNamespace
                 soundway = currentSoundway;
                 return true;
             }
+            return false;
+        }
+
+        public bool CheckSoundwayBeforeSwap(Soundway soundway)
+        {
+            // soundway.SplineContainer.KnotLinkCollection.GetKnotLinks(1);
+            return false;
+        }
+
+        public bool CheckAvailableSoundwaySwaps(Soundway soundway)
+        {
             return false;
         }
         
